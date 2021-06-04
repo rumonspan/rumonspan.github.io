fc2bbd2f39384179d63760d0d327d33a75da3f1f
rumonspan <85341156+rumonspan@users.noreply.github.com>
2021-06-04 16:39:57 +0700

    Re-upload RUM

--- rum-src/footer.js
+++ rum-src/footer.js
@@ -2,9 +2,10 @@ function footerScript(pageLevel){
 	var year = new Date().getFullYear();
 	if (pageLevel == 1){
 		document.write("<div class='copyright'>2020-"+year+"</div>");
-		document.write("<div class ='channelID'><a href='https://www.youtube.com/channel/UCC9z07qxRHUtHBVfFlETohA/' target='_blank'><img class='ytLogo' src='rum-src/yt_icon_mono_dark.png'>Rum Onspan Music</a></div></p></footer>");
-	} else if (pageLevel==2){
+		document.write("<div class ='channelID'><a href='https://www.youtube.com/channel/UCC9z07qxRHUtHBVfFlETohA/' target='_blank'><img class='ytLogo' src='rum-src/yt_icon_mono_dark.png'>Rum Onspan Music</a></div></p>");
+	} 
+	else if (pageLevel==2 || pageLevel == 1.5){
 		document.write("<div class='copyright'>2020-"+year+"</div>");
-		document.write("<div class ='channelID'><a href='https://www.youtube.com/channel/UCC9z07qxRHUtHBVfFlETohA/' target='_blank'><img class='ytLogo' src='../rum-src/yt_icon_mono_dark.png'>Rum Onspan Music</a></div></p></footer>");
+		document.write("<div class ='channelID'><a href='https://www.youtube.com/channel/UCC9z07qxRHUtHBVfFlETohA/' target='_blank'><img class='ytLogo' src='../rum-src/yt_icon_mono_dark.png'>Rum Onspan Music</a></div></p>");
 	}
 }
